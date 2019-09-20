4f164dab106bd55ad6c93c928193e09e5ffd0d7f
Petrucho <petrucho@post.cz>
2019-09-20 10:15:10 +0300

    UPD: server started; map drawing; click catched

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { GoogleAPYKey: {key: key} } = require('../constants/other-constants');
+const {
+  GoogleAPYKey: { key: key },
+} = require('../constants/other-constants');
 
 router.get('/', function(req, res, next) {
   // console.log(`key: ${key}`);
